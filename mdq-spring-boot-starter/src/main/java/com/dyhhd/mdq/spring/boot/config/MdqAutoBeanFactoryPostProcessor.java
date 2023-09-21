b7e3a3eebd9ca49c125de8500823520c2cd37b8d
ln <13137920245@163.com>
2023-09-21 19:10:16 +0800

    perf:优化消费者接口执行顺序 对实现了 Consumer 接口的实现类，执行顺序进行优化

--- mdq-spring-boot-starter/src/main/java/com/dyhhd/mdq/spring/boot/config/MdqAutoBeanFactoryPostProcessor.java
+++ mdq-spring-boot-starter/src/main/java/com/dyhhd/mdq/spring/boot/config/MdqAutoBeanFactoryPostProcessor.java
@@ -1,38 +1,76 @@
 package com.dyhhd.mdq.spring.boot.config;
 
-import com.dyhhd.mdq.core.*;
+import com.dyhhd.mdq.core.AbstractConsumer;
+import com.dyhhd.mdq.core.Consumer;
+import com.dyhhd.mdq.core.DelayQueueManage;
+import com.dyhhd.mdq.core.Producer;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+import org.springframework.util.Assert;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.DelayQueue;
 
 /**
  * 消费者 bean factory 处理器
  *
  * @author lv ning
  */
-public class MdqAutoBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+public class MdqAutoBeanFactoryPostProcessor implements SmartInitializingSingleton, BeanFactoryPostProcessor {
+
+    private ConfigurableListableBeanFactory beanFactory;
+
+    private DelayQueueManage manage;
+
+    private Producer producer;
+
+    private List<Consumer> consumers;
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        DelayQueueManage manage = beanFactory.getBean("delayQueueManage", DelayQueueManage.class);
+        this.beanFactory = beanFactory;
+
+        this.manage = beanFactory.getBean("delayQueueManage", DelayQueueManage.class);
+
+        this.producer = beanFactory.getBean("producer", Producer.class);
+
         Map<String, Consumer> beans = beanFactory.getBeansOfType(Consumer.class, false, false);
-        for (Consumer c : beans.values()) {
+        List<Consumer> consumers = new ArrayList<>(beans.values());
+        AnnotationAwareOrderComparator.sort(consumers);
+        this.consumers = consumers;
+    }
+
+
+    @Override
+    public void afterSingletonsInstantiated() {
+//        ConfigurableListableBeanFactory beanFactory = this.beanFactory;
+//        Assert.state(this.beanFactory != null, "No ConfigurableListableBeanFactory set");
+
+        DelayQueueManage manage = this.manage;
+        Assert.state(this.manage != null, "DelayQueueManage not initialized");
+
+        Producer producer = this.producer;
+        Assert.state(this.producer != null, "Producer not initialized");
+
+        List<Consumer> consumers = this.consumers;
+        Assert.state(this.consumers != null, "Consumer List not initialized");
+
+        for (Consumer c : consumers) {
             if (c instanceof AbstractConsumer) {
                 AbstractConsumer consumer = (AbstractConsumer) c;
-                DelayQueue<Worker> delayQueue = consumer.getDelayQueue();
-                if (null != delayQueue) {
+                if (null != consumer.getDelayQueue()) {
                     continue;
                 }
 
-                Producer producer = consumer.getProducer();
-                if (null != producer) {
+                if (null != consumer.getProducer()) {
                     continue;
                 }
 
-                producer = beanFactory.getBean("producer", Producer.class);
+                // 设置消费者
                 consumer.setProducer(producer);
 
                 consumer.setDelayQueue(consumer.getProducer().getQueue(consumer.getQueue()));
